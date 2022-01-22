19a648e03b3b988e2a58ad9384ec58958c0e100d
torippi <19960215yusuke@gmail.com>
2022-01-23 00:05:56 +0900

    Initial Commit

---
+++ simpleStrategy/simpleStrategyApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleStrategyApp.swift
+//  simpleStrategy
+//
+//  Created by 丹羽悠介 on 2022/01/23.
+//
+
+import SwiftUI
+
+@main
+struct simpleStrategyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
